dea33dba1ae0fab6e2ddbd59da48dadb7f5c3bf3
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-12 15:41:11 +0200

    Re-write last comment

--- BeginComments/CommentHello.java
+++ BeginComments/CommentHello.java
@@ -6,6 +6,6 @@ package BeginComments;
 
 public class CommentHello{ // The name of the class must match the file name
     public static void main(String[] args) { //program execution begins here
-        System.out.println("Hello, Universe!"); //outputs Hello, universe
+        System.out.println("Hello, Universe!"); //outputs Hello, Universe!
     }
  }
