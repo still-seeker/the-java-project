6eb171780cd192ac479b16961d74024d7a90cfc4
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-12 15:39:05 +0200

    save the commented source file

---
+++ BeginComments/CommentHello.java
@@ -0,0 +1,11 @@
+package BeginComments;
+/*
+ * This is a commented version of our basic Hello universe program.
+ * And this is a multi-line comment
+ */
+
+public class CommentHello{ // The name of the class must match the file name
+    public static void main(String[] args) { //program execution begins here
+        System.out.println("Hello, Universe!"); //outputs Hello, universe
+    }
+ }
